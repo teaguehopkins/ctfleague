c326b0ce41275a46c0f4b6db32cd5f04d31d9692
Teague Hopkins <teague@teaguehopkins.com>
2014-07-31 15:16:49 -0400

    Prevent multiple Ready clicks from breaking stuff.
    Add backend validation to prevent the same match from being simulated multiple times when someone clicks on the Ready button more than once.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,29 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+//Don't know why this stuff doesn't work. Going to use backend validation instead and come back to this later.
+
+// $( document ).ready(function() {
+
+// $('.draft_button').one('submit', function() {      $(this).find('input[type="submit"]').attr('disabled','disabled').attr('value', 'Processing...');   });
+
+//$('.draft_button').one('click', function() {      $(this).attr('disabled','true') });
+
+//$('.draft_button').on('click', function(){    $(this).attr('value', "Processing...") ;});
+
+//$('.draft_button').one('click', function() {  $(this).attr('disabled','true'); $(this).attr('value', "Processing..."); return true;  });
+
+//   $(".league_h1").attr("style", "color:red").html("Hello jQuery");
+
+//  $('.draft_button').attr('value', "FOUND");
+
+//  $( "form" ).submit(function( event ) {
+//  event.preventDefault();
+//  });
+
+
+//  $('.draft_button').on('click', function(){    $(this).attr('disabled', 'true').attr('value', "Processing...") ;});
+
+//});
