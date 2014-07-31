6db4aacfcec2c826267c2a5341898e67a1e08b00
Teague Hopkins <teague@teaguehopkins.com>
2014-07-31 17:35:55 -0400

    Fix promotion unit test.

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -6,23 +6,23 @@ describe "Match" do
 
   it "should not promote with 0 xp" do
     soldier.set_starting_attributes
-    match.promote_soldier(soldier)
+    match.send(:promote_soldier, soldier)
     soldier.rank.should be(1)
   end
 
   it "should promote soldiers who earn enough xp" do
     soldier.set_starting_attributes
     soldier.xp=1
-    match.promote_soldier(soldier)
+    match.send(:promote_soldier, soldier)
     soldier.rank.should be(4)
     soldier.xp=7
-    match.promote_soldier(soldier)
+    match.send(:promote_soldier, soldier)
     soldier.rank.should be(8)
     soldier.xp=3
-    match.promote_soldier(soldier)
+    match.send(:promote_soldier, soldier)
     soldier.rank.should be(6)
     soldier.xp=13
-    match.promote_soldier(soldier)
+    match.send(:promote_soldier, soldier)
     soldier.rank.should be(12)
   end
 
