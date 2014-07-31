3b460223c0117ab3ab87f2f1f5d0465e7f4c1020
Teague Hopkins <teague@teaguehopkins.com>
2014-07-31 15:56:59 -0400

    Move match logic from controller to model.

--- app/models/match.rb
+++ app/models/match.rb
@@ -24,13 +24,86 @@ class Match < ActiveRecord::Base
 
   def finish
     self.finished = true
+    @team_1.heal_bench_tokens
+    @team_2.heal_bench_tokens
     award_points
     increase_unit_stats
     MatchMailer.send_match_results_emails(self)
     self.save
   end
 
-  #private
+  def simulate
+    @league = self.league
+    @team_1 = self.users.first.teams.find_by_league_id(@league.id)
+    @team_2 = self.users.last.teams.find_by_league_id(@league.id)
+    @team_1.tokens.on_squad.each do |token|
+      self.match_tokens.create(:token => token, :side => 1, :xloc => 0, :direction => 1, :flag => false, :spotted => false)
+    end
+
+    @team_2.tokens.on_squad.each do |token|
+      self.match_tokens.create(:token => token, :side => 2, :xloc => 1000, :direction => -1, :flag => false, :spotted => false)
+    end
+
+      self.match_tokens.each do |match_token|
+        match_token.token.units.first.soldiers.first.update active: true
+      end
+
+    @flagwinner = nil
+    until @flagwinner != nil do
+      self.log("======================================= New Round =======================================")
+      self.match_tokens.sort { |a, b| b.init <=> a.init }.each do |match_token| #order tokens by speed
+        vis = ""
+        vis = "_____Vis" if match_token.spotted
+        vis = vis + "_____Flag" if match_token.flag
+        if match_token.side == 1 && match_token.soldier.active
+          self.log(match_token.soldier.last_name + " at " + match_token.xloc.to_s + " " + vis)
+        end
+        if match_token.side == 2 && match_token.soldier.active
+          self.log("_____________________________________________" + match_token.soldier.last_name + " at " + match_token.xloc.to_s + " " + vis)
+        end
+      end
+    self.log("--------------------------------------- Actions ---------------------------------------")
+      sim_one_turn
+    end
+      @flagwinner.winner = true
+      @flagwinner.save
+  end
+
+  private
+
+  def sim_one_turn
+    self.match_tokens.each do |match_token|
+      if match_token.soldier.active
+        match_token.check_sights
+        if match_token.opponents_visible
+          if match_token.can_hit(match_token.nearest_opponent)
+            match_token.shoot(match_token.nearest_opponent)
+          else
+            match_token.run
+            match_token.flag_grab
+            flag_winner(match_token)
+          end
+        else
+          match_token.run
+          match_token.flag_grab
+          flag_winner(match_token)
+        end
+      end
+    end
+  end
+
+  def flag_winner(match_token)
+    #check flag win conditions - does not handle ties - ties go to side 1
+    if match_token.flag && match_token.xloc == 0 && match_token.side == 1
+      self.log(match_token.soldier.last_name + " has captured the flag!")
+      @flagwinner = self.match_members.where(user_id: @team_1.user.id).first
+      self.log(@team_1.name + " wins!")
+    elsif match_token.flag && match_token.xloc == 1000 && match_token.side == 2
+      self.log(match_token.soldier.last_name + " has captured the flag!")
+      self.log(@team_2.name + " wins!")
+      @flagwinner = self.match_members.where(user_id: @team_2.user.id).first
+    end
+  end
 
   def award_points
     self.match_members.each do |match_member|
